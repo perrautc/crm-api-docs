16fd41a570d6bee24899c700d03da91664a4687f
Charles Perraut <charles.perraut@gmail.com>
2023-01-03 06:43:00 -0500

    updated configs

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,14 +4,11 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/perrautc/crm-api-docs',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/perrautc/crm-api-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'CRM API Docs',
   },
 }
 
